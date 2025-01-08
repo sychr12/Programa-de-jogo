61696116b9ba8c2f5b68107c71774c058b3121a7
Luiz Felipe <sayychr@gmail.com>
2025-01-08 09:18:16 -0400

    Add files via upload

---
+++ Programadejg/gui/janela.java
@@ -0,0 +1,15 @@
+package gui;
+
+public class janela {
+    
+package gui;
+import javax.swing.JFrame;
+
+public class janela extends JFrame{
+    public janela(){
+        this.setBounds(0,0, 1920, 1080);
+        this.setVisible(true);
+        
+    }
+    
+}
